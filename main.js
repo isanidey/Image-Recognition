b1e6caaa7ad657f8951f821099ed8b44c5013134
Isani Love <74708250+isanidey@users.noreply.github.com>
2021-05-05 17:34:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/j6wCV-6d1/',modelLoaded);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/j6wCV-6d1/model.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Model Loaded!');
